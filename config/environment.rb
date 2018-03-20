9ca150b8ebbbeea7fb34b21e28041c2a4a75826b
Krystof Dostal <kdostal@centrum.cz>
2018-03-20 16:20:27 -0400

    Added mailer settings to environment file

--- config/environment.rb
+++ config/environment.rb
@@ -9,7 +9,7 @@ ActionMailer::Base.smtp_settings = {
   :address        => ENV['MAILGUN_SMTP_SERVER'],
   :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
   :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-  :domain         => 'thawing-meadow-66253.herokuapp.com',
+  :domain         => 'gentle-plateau-20141.herokuapp.com',
   :authentication => :plain,
 }
 ActionMailer::Base.delivery_method = :smtp
